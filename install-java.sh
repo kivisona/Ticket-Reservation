022c2cf6720146d8ad29e7be850db80fece191ca
kivisona <kivisona13@gmail.com>
2025-03-19 08:57:09 +0530

    Update install-java.sh

--- install-java.sh
+++ install-java.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 echo "Installing Java 23..."
-wget https://download.java.net/java/GA/jdk23/latest/jdk-23_linux-x64_bin.tar.gz -O jdk23.tar.gz
-tar -xzf jdk23.tar.gz
-mv jdk-23 /opt/java-23
+
+# Create installation directory
+mkdir -p /opt/java-23
+
+# Download Java 23 from a working source
+wget https://download.bell-sw.com/java/23/bellsoft-jdk23-linux-amd64.tar.gz -O java23.tar.gz
+
+# Extract Java
+tar -xzf java23.tar.gz -C /opt/java-23 --strip-components=1
+
+# Set JAVA_HOME
 export JAVA_HOME=/opt/java-23
 export PATH=$JAVA_HOME/bin:$PATH
+
 echo "Java 23 installed successfully!"
+java -version
